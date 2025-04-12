1c539c58101f57421e1f9ea385493d5aff584b30
Agus <56741649+apulise@users.noreply.github.com>
2025-04-12 13:51:51 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,187 @@
+# @title ##**[❗]  Set up**
+# @markdown ###**Setup requirements** {display-mode: "form"}
+# @markdown Check out [wiki of this project](https://minecolabimproved.wiki.gg/es/wiki/MineColab_Improved_Wiki) for more explanations
+import requests
+from requests import get
+import sys
+from IPython.display import Javascript, display
+from google.colab import output, files
+#------------------------------------------------------------------------------------------------------------------------------------#
+colabversion="0.2.7.1"
+setup_run=True
+#------------------------------------------------------------------------------------------------------------------------------------#
+# Fetching the latest version to give an alert if out of date!
+url = "https://raw.githubusercontent.com/Prozoon700/MI-Test/main/version.json"
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    latest_version = data[0]['latest_version']
+    print(f"Latest version available: {latest_version}")
+    user_decision=None
+    versionok=None
+    if latest_version != colabversion:
+        user_decision = output.eval_js("window.confirm('Version out of date. Do you want to continue using this version?')")
+        versionok=False
+    else:
+      print("Yehaa! Version up to date, you can continue using this notebook!")
+      versionok=True
+except Exception as e:
+    print("Error trying to obtain the latest version... Check that the notebook is up to date.")
+    raise Exception(e)
+else:
+  if not user_decision and versionok==False:
+    raise Exception("Execution stopped: Version out of date. Please update MineColab from discord/github")
+  elif user_decision and versionok==True:
+    print("Warning: Using old version. Compatibility is not guaranteed.")
+
+print("Downloading required libraries... Please wait.")
+
+from time import sleep
+from json import load, dump
+from os.path import exists
+from os import makedirs
+from IPython.display import clear_output
+%pip install -q jproperties
+%pip install -q rich
+from rich import print
+%pip install -q pyngrok
+%pip install -q pyngrok
+%pip install -q BeautifulSoup4
+%pip install -q ruamel.yaml
+%pip install -q jupyter-ui-poll
+%pip install -q progress
+%pip install -q mcstatus
+import subprocess
+from progress.spinner import Spinner
+if exists('/content/drive') == False:
+  from google.colab import drive
+  drive.mount('/content/drive')
+drive_path = '/content/drive/MyDrive/minecraft'
+SERVERCONFIG = f'{drive_path}/server_list.txt'
+def LOG(*args, sep=''):
+  check = False
+  args = list(args)
+  for i in range(len(args)):
+    args[i] = str(args[i])
+    if '\n' in args[i]: args[i] = args[i].replace('\n', ''); args.insert(0, '\n[bold green][ LOG ][/bold green] '); check = True; break
+  if check == False: args.insert(0, '[bold green][ LOG ][/bold green] ')
+  print(sep.join(map(str, args)))
+
+def WARN(*args, sep=''):
+  check = False
+  args = list(args)
+  for i in range(len(args)):
+    args[i] = str(args[i])
+    if '\n' in args[i]: args[i] = args[i].replace('\n', ''); args.insert(0, '\n[bold light_goldenrod1][ WARN ][/bold light_goldenrod1] '); check = True; break
+  if check == False: args.insert(0, '[bold light_goldenrod1][ WARN ][/bold light_goldenrod1] ')
+  print(sep.join(map(str, args)))
+#---------------------------------------------------------------------------------- Print Box ------------------------------------------------------------------------------------------#
+def print_msg_box(msg, indent=1, width=None, title=None):
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    border_color = "[bold gold3]"
+    title_color = "[bold cyan]"
+    text_color = "[bold white]"
+    header_color = "[bold light_goldenrod1]"
+    reset_color = "[/]"
+    box = f'{border_color}+{"-" * (width + indent * 2)}+{reset_color}\n'  # upper_border
+    if title:
+        box += f'{border_color}|{space}{title_color}{title:<{width}}{reset_color}{space}{border_color}|\n'  # title
+        box += f'{border_color}|{space}{"-" * len(title):<{width}}{space}{border_color}|\n'  # underscore
+    box += ''.join([f'{border_color}|{space}{text_color}{line:<{width}}{reset_color}{space}{border_color}|\n' for line in lines])
+    box += f'{border_color}+{"-" * (width + indent * 2)}+{reset_color}'  # lower_border
+    print(box)
+#------------------------------------------------------------------------------------------------------------------------------------#
+log = "echo -e '\e[92m[ LOG ]\e[0m'" # Log function for bash
+def ERROR(*args, sep=''):
+  %cd $drive_path
+  clear_output()
+  check = False
+  args = list(args)
+  for i in range(len(args)):
+    args[i] = str(args[i])
+    if '\n' in args[i]: args[i] = args[i].replace('\n', ''); args.insert(0, '\n[ ERROR ] '); check = True; break
+  if check == False: args.insert(0, '[ ERROR ] ')
+  raise Exception(sep.join(map(str, args)))
+def INFO(*args, sep=''):
+  check = False
+  args = list(args)
+  for i in range(len(args)):
+    args[i] = str(args[i])
+    if '\n' in args[i]: args[i] = args[i].replace('\n', ''); args.insert(0, '\n[bold blue][ INFO ][/bold blue] '); check = True; break
+  if check == False: args.insert(0, '[bold blue][ INFO ][/bold blue] ')
+  print(sep.join(map(str, args)))
+def MKDIR(path):
+  try:
+    makedirs(path, exist_ok = True)
+    LOG(f'Directory {path} created')
+  except: ERROR(f'Directory {path} already existed')
+def DOWNLOAD_FILE(url, path, file_name, force = False,headers=None):
+  # Check gate
+  if not force:
+    if exists( path+ '/' + file_name): LOG(f'File {file_name} already existed')
+    else:
+      # Download file into file_name thourgh url
+      r = GET(url,headers=headers)
+      LOG('\nDownloading ' + file_name)
+      with open(path + '/' + file_name, 'wb') as f:
+        f.write(r.content)
+  else:
+    r = GET(url,headers=headers)
+    LOG('\nDownloading ' + file_name)
+    with open(path + '/' + file_name, 'wb') as f:
+      f.write(r.content)
+def GET(url,headers=None):
+  r = get(url,headers=headers)
+  # Check gate
+  if r.status_code == 200:
+    return r
+  else: ERROR('Error '+ str(r.status_code) + "! Most likely you entered an unsupported version. Try running the code again if you think that shouldn't have happened.")
+
+def COLABCONFIG(server_name):
+  return f"{drive_path}/{server_name}/colabconfig.txt"
+def COLABCONFIG_LOAD(server_name):
+  if exists(COLABCONFIG(server_name)): return load(open(COLABCONFIG(server_name)))
+  else: ERROR('Please checking whether you deleted your colabconfig file or not.')
+
+def SERVER_IN_USE(server_name):
+  if exists(f'{drive_path}/{server_name}') and server_name != '': return server_name
+  else:
+    serverconfig = load(open(SERVERCONFIG))
+    if serverconfig['server_in_use'] != '': return serverconfig['server_in_use']
+    else: ERROR('Please create a minecraft server first!')
+# The jar file name
+def JAR_LIST_RUN(server_version):
+  return {'generic': 'server.jar', 'vanilla':'server.jar','snapshot': 'server.jar',   # NORMAL
+          'purpur' : 'server.jar', 'paper': 'server.jar', 'velocity' : 'server.jar', 'folia': 'server.jar',  # PLUGINS
+          'fabric' : 'server.jar',  # forge doesn't include in this category           # MODS
+          'arclight' : 'server.jar', 'mohist': 'server.jar', 'banner': 'server.jar'} # HYBRID
+def JavaUrl(build, version):
+  if build == "Corretto":
+    return f"https://corretto.aws/downloads/latest/amazon-corretto-{version}-x64-linux-jdk.deb"
+  elif build == "Azul Zulu":
+    return [i["download_url"] for i in GET("https://api.azul.com/metadata/v1/zulu/packages?availability_types=ca&release_status=both&page_size=1000&include_fields=os,arch,java_package_type,archive_type&page=1&azul_com=true").json() if i["archive_type"] == "deb" and i["java_package_type"] == "jre" and "amd64" in i["name"] and not "fx" in i["name"] and i["java_version"] == list(map(int, version.split('.')))]
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+LOG(f"\nColab Version: {colabversion}")
+LOG("Gettings VM Info")
+ipinfo=requests.get("https://ipinfo.io/")
+ipinfo=ipinfo.json()
+print_msg_box(f"IP: {ipinfo['ip']}\nCity: {ipinfo['city']}\nRegion: {ipinfo['region']}\nCountry: {ipinfo['country']}", indent=4, width=25, title="Colab VM Information")
+LOG("Upgrading VM packages")
+!sudo apt update &>/dev/null &&echo 'apt cache successfully updated' || echo "apt cache update failed, you might receive stale packages"
+!sudo apt upgrade &>/dev/null &&echo 'apt cache successfully upgraded' || echo "apt cache upgrade failed, you might receive stale packages"
+!export DISPLAY=:0.0
+if exists(drive_path) == False:
+  !mkdir -p $drive_path &>/dev/null&
+  sleep(40)
+if exists(SERVERCONFIG) == False:
+  dump({"server_list": [], "server_in_use": "", "ngrok_proxy" : {'authtoken' : '', "region" : ''}, "zrok_proxy": {"authtoken": ''}, 'localtonet_proxy': {"authtoken": ''}, 'localxpose_proxy': {"authtoken": ''}}, open(SERVERCONFIG, 'w'))
+%cd $drive_path
+LOG('Completed')
